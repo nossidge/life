b5fcd5b3db8b8cf087de4042c6147c9ad926af95
Paul Thompson <nossidge@gmail.com>
2017-12-31 00:41:07 +0000

    move drawScene into ANIMATION

--- animation.js
+++ animation.js
@@ -15,7 +15,7 @@ var ANIMATION = ( function(mod) {
     animation = window.requestAnimationFrame(callback);
   }
 
-  // Draw the first scene.
+  // Draw the first scene, and initialise the animation loop.
   mod.initCanvas = function() {
     ANIMATION.cancelAnimationFrame();
     then = Date.now();
@@ -42,7 +42,51 @@ var ANIMATION = ( function(mod) {
     };
 
     CANVAS.randomiseCentralBlock();
-    drawScene();
+    ANIMATION.drawScene();
+  }
+
+  // Iterate the animation loop based on time delta.
+  mod.drawScene = function() {
+    ANIMATION.requestAnimationFrame(ANIMATION.drawScene);
+    now = Date.now();
+    delta = now - then;
+    if (!STATE.paused() && delta > interval || STATE.stepToNextFrame()) {
+      then = now - (delta % interval);
+      STATE.stepToNextFrame(false);
+
+      dead = FUNCTIONS.hexToRgb(Cell.get_fillColourDead());
+      let rgb = dead.r + ', ' + dead.g + ', ' + dead.b;
+      CANVAS.c.fillStyle = 'rgba(' + rgb + ', ' + STATE.blurAbsolute() + ')';
+      CANVAS.c.fillRect(0,0,w,h);
+
+      // Display cells.
+      for(var i = 0; i < Cell.get_cellCount().x; i++) {
+        for(var j = 0; j < Cell.get_cellCount().y; j++) {
+          cells[i][j].render(CANVAS.c);
+        }
+      }
+
+      // Calculate next state.
+      globalStateStatic = true;
+      for(var i = 0; i < Cell.get_cellCount().x; i++) {
+        for(var j = 0; j < Cell.get_cellCount().y; j++) {
+          var state = !nextStateAccordingToNeighbours(i,j) ? 0 : 1;
+          if (cells[i][j].getStateNext() != state) {
+            cells[i][j].setStateNext(state);
+            globalStateStatic = false;
+          }
+        }
+      }
+
+      // Have we reached a no-change state?
+      // (Might use this in the future...)
+      if (globalStateStatic == true) {
+        FUNCTIONS.puts('dead');
+      }
+
+      // Move to next rule in the loop, if necessary.
+      STATE.frameCountTick();
+    }
   }
 
   return mod;
--- life.js
+++ life.js
@@ -107,52 +107,6 @@ function checkLifeRules() {
 
 //##############################################################################
 
-//functionality
-// http://codetheory.in/controlling-the-frame-rate-with-requestanimationframe/
-function drawScene() {
-  ANIMATION.requestAnimationFrame(drawScene);
-  now = Date.now();
-  delta = now - then;
-  if (!STATE.paused() && delta > interval || STATE.stepToNextFrame()) {
-    then = now - (delta % interval);
-    STATE.stepToNextFrame(false);
-
-    dead = FUNCTIONS.hexToRgb(Cell.get_fillColourDead());
-    CANVAS.c.fillStyle = 'rgba(' + dead.r + ', ' + dead.g + ', ' + dead.b + ', ' + STATE.blurAbsolute() + ')';
-    CANVAS.c.fillRect(0,0,w,h);
-
-    // Display cells.
-    for(var i = 0; i < Cell.get_cellCount().x; i++) {
-      for(var j = 0; j < Cell.get_cellCount().y; j++) {
-        cells[i][j].render(CANVAS.c);
-      }
-    }
-
-    // Calculate next state.
-    globalStateStatic = true;
-    for(var i = 0; i < Cell.get_cellCount().x; i++) {
-      for(var j = 0; j < Cell.get_cellCount().y; j++) {
-        var state = !nextStateAccordingToNeighbours(i,j) ? 0 : 1;
-        if (cells[i][j].getStateNext() != state) {
-          cells[i][j].setStateNext(state);
-          globalStateStatic = false;
-        }
-      }
-    }
-
-    // Have we reached a no-change state?
-    // (Might use this in the future...)
-    if (globalStateStatic == true) {
-      FUNCTIONS.puts('dead');
-    }
-
-    // Move to next rule in the loop, if necessary.
-    STATE.frameCountTick();
-  }
-}
-
-//##############################################################################
-
 // Init function.
 (function() {
   let cellCount  = Cell.get_cellCount();
