a8eccbe2ce531ee1246d87dec48b69d7a8ab0698
Paul Thompson <nossidge@gmail.com>
2018-01-30 16:48:35 +0000

    fix mouse clicks in Firefox

--- app/scripts/mouse.js
+++ app/scripts/mouse.js
@@ -6,13 +6,34 @@
 var MOUSE = ( function(mod) {
 
   let pos = {x: 0, y: 0};
+  let mouseDown = false;
+  let clickType = 'LEFT';
+
+  // Set mouse down bool.
+  mod.mouseDown = function(value) {
+    mouseDown = value;
+  }
+
+  // Determine which mouse click was performed.
+  // http://www.javascripter.net/faq/leftvsrightmousebutton.htm
+  mod.setClickType = function(e) {
+    let evt = (e == null ? event : e);
+    clickType = 'LEFT';
+    if (evt.which) {
+      if (evt.which == 3) clickType = 'RIGHT';
+      if (evt.which == 2) clickType = 'MIDDLE';
+    } else if (evt.button) {
+      if (evt.button == 2) clickType = 'RIGHT';
+      if (evt.button == 4) clickType = 'MIDDLE';
+    }
+  }
 
   // Draw a cell to the canvas if the mouse is pressed.
   mod.drawCellFromMousePos = function(e) {
-    if (e.which != 0) {
+    if (mouseDown) {
 
       // Left click for live, middle click for dead.
-      let state = (e.which == 2) ? 0 : 1;
+      let state = (clickType == 'LEFT') ? 1 : 0;
 
       // Determine cell x/y from mouse x/y
       pos.x = e.pageX - CANVAS.a.offsetLeft;
@@ -40,8 +61,15 @@ var MOUSE = ( function(mod) {
 
 // Draw if clicked, or if mouse dragged while clicked.
 CANVAS.a.addEventListener('mousedown', function(e) {
+  MOUSE.mouseDown(true);
+  MOUSE.setClickType(e);
   MOUSE.drawCellFromMousePos(e);
 }, false);
+
+CANVAS.a.addEventListener('mouseup', function(e) {
+  MOUSE.mouseDown(false);
+}, false);
+
 CANVAS.a.addEventListener('mousemove', function(e) {
   MOUSE.drawCellFromMousePos(e);
 }, false);
